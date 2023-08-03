f8436d7fea8fb06d95109e35b3f8116fe8635cc7
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-08-03 09:35:08 +0900

    20230803_eunhui
    "가운데 글자 가져오기" 코딩 테스트 연습

---
+++ 가운데글자가져오기.js
@@ -0,0 +1,25 @@
+function solution(s) {
+  var answer = "";
+  let startIndex = 0;
+
+  // 문자열이 짝수인 지 홀수인 지 판단
+  const evenOddType = s.length % 2;
+
+  // 문자열이 홀수라면
+  if (evenOddType === 1) {
+    startIndex = Math.floor(s.length / 2);
+
+    answer = s.slice(startIndex, startIndex + 1);
+  }
+  // 문자열이 짝수라면
+  else {
+    startIndex = s.length / 2 - 1;
+
+    answer = s.slice(startIndex, startIndex + 2);
+  }
+
+  return answer;
+}
+
+solution("abcde");
+solution("qwer");
