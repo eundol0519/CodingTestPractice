dcfa8c553cf45494d8533e91093071834921b01c
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-09-13 13:21:15 +0900

    📰 : 구름 레벨_구름 크기 측정하기

---
+++ Goorm_Level/구름크기측정하기.js
@@ -0,0 +1,9 @@
+function solution(n, m) {
+  return n * m;
+}
+
+solution(3, 4);
+solution(5, 3);
+
+// 링크 : https://level.goorm.io/exam/150332/%EA%B5%AC%EB%A6%84-%ED%81%AC%EA%B8%B0-%EC%B8%A1%EC%A0%95%ED%95%98%EA%B8%B0/quiz/1
+// 해당 링크에서는 Node.js 버전으로 되어 있어서 JS용으로 작성하였습니다.
